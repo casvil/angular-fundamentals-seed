a264f960aab8f610177453abec04a55eb5d62a81
David Casanellas <david.casanellas@weareblueorange.com>
2017-11-08 17:57:32 +0100

    chore: Injectable decorator and HttpModule

--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -1,7 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
 import { Passenger } from './models';
 
+@Injectable()
 export class PassengerDashboardService {
-  constructor() {}
+  constructor(private http: Http) {}
 
   getPassengers(): Passenger[] {
     return [{
