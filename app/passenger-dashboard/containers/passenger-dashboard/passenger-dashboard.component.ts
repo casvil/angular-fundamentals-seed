0ff85ffea043a811aa39f279d8445d75748a06cc
David Casanellas <david.casanellas@weareblueorange.com>
2017-11-08 18:50:56 +0100

    chore: Http put, delete with immutable state plus add ES6 for Object.assign property

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -32,20 +32,30 @@ export class PassengerDashboardComponent implements OnInit {
 
   ngOnInit() {
     this.passengerService
-    .getPassengers()
-    .subscribe((data: Passenger[]) => this.passengers = data);
+      .getPassengers()
+      .subscribe((data: Passenger[]) => this.passengers = data);
   }
 
   handleEdit(event: Passenger) {
-    this.passengers = this.passengers.map((passenger: Passenger) => {
-      if (passenger.id === event.id) {
-        passenger = Object.assign({}, passenger, event);
-      }
-      return passenger;
-    });
+    this.passengerService
+      .updatePassengers(event)
+      .subscribe((data: Passenger) => {
+        console.log(data, 'edited user')
+
+        this.passengers = this.passengers.map((passenger: Passenger) => {
+          if (passenger.id === event.id) {
+            passenger = Object.assign({}, passenger, event);
+          }
+          return passenger;
+        });
+      })
   }
 
   handleRemove(event: Passenger) {
-    this.passengers = this.passengers.filter((passenger: Passenger) => passenger.id !== event.id);
+    this.passengerService
+      .removePassengers(event)
+      .subscribe((data: Passenger) => {
+        this.passengers = this.passengers.filter((passenger: Passenger) => passenger.id !== event.id);
+      })
   }
 }
